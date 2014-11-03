d9c204dd253dba182c471b43fe852ab705af035a
Doug Knight <dougbknight@gmail.com>
2014-11-03 16:01:20 -0600

    added deck function

--- src/clojure_cards/core.clj
+++ src/clojure_cards/core.clj
@@ -1,6 +1,15 @@
 (ns clojure-cards.core
   (:gen-class))
 
+(def suits [:clubs :diamonds :hearts :spades])
+(def ranks [:ace 2 3 4 5 6 7 8 9 10 :jack :queen :king])
+
+(defn deck
+  "Returns a new unshuffled deck"
+  []
+  (for [suit suits rank ranks]
+    [suit rank]))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
--- test/clojure_cards/core_test.clj
+++ test/clojure_cards/core_test.clj
@@ -1,7 +1,7 @@
 (ns clojure-cards.core-test
   (:require [clojure.test :refer :all]
-            [clojure-cards.core :refer :all]))
+            [clojure-cards.core :refer :all :as cards]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest deck-has-52-cards
+  (testing "A new deck should have 52 cards"
+    (is (= 52 (count (cards/deck))))))
